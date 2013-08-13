025f780da54155b283c16299ec4caf8b8ee6cb1e
Alex-Cosma <alexx.cosma@gmail.com>
2013-08-13 16:37:29 +0200

    Starting imdb api

---
+++ src/groovy/com/movie/feel/apis/ImdbApi.groovy
@@ -0,0 +1,11 @@
+package com.movie.feel.apis
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 8/13/13
+ * Time: 4:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ImdbApi {
+}
